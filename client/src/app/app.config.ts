f2a5ef113753580c886a161484ce10c55536ebad
metepg <mete.guneysel@gmail.com>
2025-02-22 16:46:20 +0200

    Add user details form

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,11 +3,18 @@ import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withHashLocation())
+    provideRouter(routes, withHashLocation()),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ]
 };
--- client/src/app/user-details/user-details.ts
+++ client/src/app/user-details/user-details.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputNumber } from 'primeng/inputnumber';
+import { FloatLabel } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    CardModule,
+    InputNumber,
+    FloatLabel,
+  ],
   templateUrl: './user-details.html',
   styleUrl: './user-details.css'
 })
+export class UserDetails {
+  userForm: FormGroup;
 
-export class UserDetails implements OnInit {
+ constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      netIncome: [0, [Validators.required, Validators.min(1)]],
+      otherIncome: [0, [Validators.required]],
+      savingsGoal: [0, [Validators.required]]
+    });
+  }
 
-  constructor() {}
-
-  ngOnInit() {}
+  onSubmit() {
+    if (this.userForm.valid) {
+      console.log('Form Submitted:', this.userForm.value);
+    }
+  }
 }
